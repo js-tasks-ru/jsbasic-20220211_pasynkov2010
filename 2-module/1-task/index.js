76aef1e9d2b8f1f0c086365adda2adac7479fc2a
Andrew Pasynkov <pasynkov2012@yandex.ru>
2022-03-02 19:42:22 +0300

    2-module 1st task

--- 2-module/1-task/index.js
+++ 2-module/1-task/index.js
@@ -1,3 +1,11 @@
 function sumSalary(salaries) {
-  // ваш код...
+  let total = 0;
+  for (let i in salaries) {
+    typeof salaries[i] === "number" &&
+    Number.isFinite(salaries[i]) &&
+    salaries[i] !== Number.isNaN(salaries[i])
+      ? (total += salaries[i])
+      : "";
+  }
+  return total;
 }

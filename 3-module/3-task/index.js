1675e5a91cb3b188d69838eede90dca46595adcc
Andrew Pasynkov <pasynkov2012@yandex.ru>
2022-03-04 00:30:59 +0300

    3-module 3-task fix variable names

--- 3-module/3-task/index.js
+++ 3-module/3-task/index.js
@@ -1,10 +1,10 @@
 function camelize(str) {
   return str
-    .split('')
-    .reduce((prev, accum) => {
-      if (prev.endsWith('-')) {
-        return prev.replace('-', accum.toUpperCase());
-      }
-      return prev + accum;
-    }, '');
+    .split("")
+    .reduce((prevVal, curVal) => {
+    if (prevVal.endsWith("-")) {
+      return prevVal.replace("-", curVal.toUpperCase());
+    }
+    return prevVal + curVal;
+  }, "");
 }

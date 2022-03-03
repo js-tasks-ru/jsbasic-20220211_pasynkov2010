b5b9515207a593a0f390058ac4a6539a2ac73dba
Andrew Pasynkov <pasynkov2012@yandex.ru>
2022-03-03 16:46:51 +0300

    2-module 1st task refactored

--- 2-module/1-task/index.js
+++ 2-module/1-task/index.js
@@ -1,11 +1,13 @@
 function sumSalary(salaries) {
   let total = 0;
   for (let i in salaries) {
-    typeof salaries[i] === "number" &&
-    Number.isFinite(salaries[i]) &&
-    salaries[i] !== Number.isNaN(salaries[i])
-      ? (total += salaries[i])
-      : "";
+    if (
+      typeof salaries[i] === "number" &&
+      Number.isFinite(salaries[i]) &&
+      salaries[i] !== Number.isNaN(salaries[i])
+    ) {
+      total += salaries[i];
+    }
   }
   return total;
 }
